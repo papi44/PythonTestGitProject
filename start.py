1eff1d3ec29b5443866595309b69a309869aa61a
papi4 <supercall1@yandex.ru>
2019-02-14 21:19:40 +0300

    Add system info output

--- start.py
+++ start.py
@@ -1,1 +1,13 @@
+
+Import platform
+
+
+
 print('pythontestproject')
+
+
+print(platform.system())
+print(platform.platform())
+print(platform.machine())
+print(platform.version ())
+print(platform.processor())
